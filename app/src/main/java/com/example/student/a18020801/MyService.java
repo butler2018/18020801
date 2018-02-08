4589e244da8db0f0c209c6ae79c2042221ca7480
calvin <butler2018@hotmail.com>
2018-02-08 10:10:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a18020801/MyService.java
@@ -0,0 +1,30 @@
+package com.example.student.a18020801;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "This is onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "This is onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
